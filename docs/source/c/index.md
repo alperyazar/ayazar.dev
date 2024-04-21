a61640b8c7768744304f02f588baa7c8294e84b0
Alper Yazar <alperyazar@gmail.com>
2024-04-21 17:55:48 +0300

    c

--- docs/source/c/index.md
+++ docs/source/c/index.md
@@ -1,4 +1,4 @@
-# 🇨
+# 🇨 C
 
 The C programming language.
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ html_copy_source = False
 
 # https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 html_theme_options = {
-#    'announcement': 'Moving from alperyazar.com...',
+    'announcement': 'Web sitemi tekrar düzenliyorum. Eksik içerikler, çalışmayan bağlantılar olabilir. 😇',
     'source_repository': 'https://github.com/alperyazar/ayazar.dev/',
     'source_branch': 'master',
     'source_directory': 'docs/source/',
